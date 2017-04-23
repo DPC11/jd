e79e6d6f927ce14c92d29e1373008f96e38517e6
DPC <dpc11@qq.com>
2017-04-23 10:38:27 +0800

    在修复源代码的小问题和调整预测阈值之后达到了 0.092。

--- gen_feat.py
+++ gen_feat.py
@@ -9,12 +9,12 @@ import os
 import math
 import numpy as np
 
-action_1_path = "./data/JData_Action_201602.csv"
-action_2_path = "./data/JData_Action_201603.csv"
-action_3_path = "./data/JData_Action_201604.csv"
-comment_path = "./data/JData_Comment.csv"
-product_path = "./data/JData_Product.csv"
-user_path = "./data/JData_User.csv"
+action_1_path = "./data.nosync/JData_Action_201602.csv"
+action_2_path = "./data.nosync/JData_Action_201603.csv"
+action_3_path = "./data.nosync/JData_Action_201604.csv"
+comment_path = "./data.nosync/JData_Comment.csv"
+product_path = "./data.nosync/JData_Product.csv"
+user_path = "./data.nosync/JData_User.csv"
 
 comment_date = ["2016-02-01", "2016-02-08", "2016-02-15", "2016-02-22", "2016-02-29", "2016-03-07", "2016-03-14",
                 "2016-03-21", "2016-03-28",
@@ -39,8 +39,9 @@ def convert_age(age_str):
     else:
         return -1
 
+
 def get_basic_user_feat():
-    dump_path = './cache/basic_user.pkl'
+    dump_path = './data.nosync/cache/basic_user.pkl'
     if os.path.exists(dump_path):
         user = pickle.load(open(dump_path))
     else:
@@ -48,14 +49,13 @@ def get_basic_user_feat():
         user['age'] = user['age'].map(convert_age)
         age_df = pd.get_dummies(user["age"], prefix="age")
         sex_df = pd.get_dummies(user["sex"], prefix="sex")
-        user_lv_df = pd.get_dummies(user["user_lv_cd"], prefix="user_lv_cd")
-        user = pd.concat([user['user_id'], age_df, sex_df, user_lv_df], axis=1)
-        pickle.dump(user, open(dump_path, 'w'))
+        user = pd.concat([user['user_id'], age_df, sex_df, user["user_lv_cd"]], axis=1)
+        # pickle.dump(user, open(dump_path, 'w'))
     return user
 
 
 def get_basic_product_feat():
-    dump_path = './cache/basic_product.pkl'
+    dump_path = './data.nosync/cache/basic_product.pkl'
     if os.path.exists(dump_path):
         product = pickle.load(open(dump_path))
     else:
@@ -64,7 +64,7 @@ def get_basic_product_feat():
         attr2_df = pd.get_dummies(product["a2"], prefix="a2")
         attr3_df = pd.get_dummies(product["a3"], prefix="a3")
         product = pd.concat([product[['sku_id', 'cate', 'brand']], attr1_df, attr2_df, attr3_df], axis=1)
-        pickle.dump(product, open(dump_path, 'w'))
+        # pickle.dump(product, open(dump_path, 'w'))
     return product
 
 
@@ -72,9 +72,11 @@ def get_actions_1():
     action = pd.read_csv(action_1_path)
     return action
 
+
 def get_actions_2():
     action2 = pd.read_csv(action_2_path)
     return action2
+
 
 def get_actions_3():
     action3 = pd.read_csv(action_3_path)
@@ -88,45 +90,46 @@ def get_actions(start_date, end_date):
     :param end_date:
     :return: actions: pd.Dataframe
     """
-    dump_path = './cache/all_action_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/all_action_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
         action_1 = get_actions_1()
         action_2 = get_actions_2()
         action_3 = get_actions_3()
-        actions = pd.concat([action_1, action_2, action_3]) # type: pd.DataFrame
+        actions = pd.concat([action_1, action_2, action_3])
         actions = actions[(actions.time >= start_date) & (actions.time < end_date)]
         pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
-def get_action_feat(start_date, end_date):
-    dump_path = './cache/action_accumulate_%s_%s.pkl' % (start_date, end_date)
+def get_action_feat(start_date, end_date, days):
+    dump_path = './data.nosync/cache/action_accumulate_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
         actions = get_actions(start_date, end_date)
         actions = actions[['user_id', 'sku_id', 'type']]
-        df = pd.get_dummies(actions['type'], prefix='%s-%s-action' % (start_date, end_date))
-        actions = pd.concat([actions, df], axis=1)  # type: pd.DataFrame
+        df = pd.get_dummies(actions['type'], prefix='%s-action' % days)
+        actions = pd.concat([actions, df], axis=1)
         actions = actions.groupby(['user_id', 'sku_id'], as_index=False).sum()
         del actions['type']
-        pickle.dump(actions, open(dump_path, 'w'))
+        # pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
 def get_accumulate_action_feat(start_date, end_date):
-    dump_path = './cache/action_accumulate_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/action_accumulate_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
         actions = get_actions(start_date, end_date)
         df = pd.get_dummies(actions['type'], prefix='action')
-        actions = pd.concat([actions, df], axis=1) # type: pd.DataFrame
-        #近期行为按时间衰减
-        actions['weights'] = actions['time'].map(lambda x: datetime.strptime(end_date, '%Y-%m-%d') - datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-        #actions['weights'] = time.strptime(end_date, '%Y-%m-%d') - actions['datetime']
+        actions = pd.concat([actions, df], axis=1)
+        # 近期行为按时间衰减
+        actions['weights'] = actions['time'].map(lambda x: datetime.strptime(end_date, '%Y-%m-%d') -
+                                                           datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        # actions['weights'] = time.strptime(end_date, '%Y-%m-%d') - actions['datetime']
         actions['weights'] = actions['weights'].map(lambda x: math.exp(-x.days))
         print actions.head(10)
         actions['action_1'] = actions['action_1'] * actions['weights']
@@ -141,12 +144,12 @@ def get_accumulate_action_feat(start_date, end_date):
         del actions['datetime']
         del actions['weights']
         actions = actions.groupby(['user_id', 'sku_id', 'cate', 'brand'], as_index=False).sum()
-        pickle.dump(actions, open(dump_path, 'w'))
+        # pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
 def get_comments_product_feat(start_date, end_date):
-    dump_path = './cache/comments_accumulate_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/comments_accumulate_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         comments = pickle.load(open(dump_path))
     else:
@@ -158,19 +161,19 @@ def get_comments_product_feat(start_date, end_date):
                 comment_date_begin = date
                 break
         comments = comments[(comments.dt >= comment_date_begin) & (comments.dt < comment_date_end)]
-        df = pd.get_dummies(comments['comment_num'], prefix='comment_num')
-        comments = pd.concat([comments, df], axis=1) # type: pd.DataFrame
-        #del comments['dt']
-        #del comments['comment_num']
-        comments = comments[['sku_id', 'has_bad_comment', 'bad_comment_rate', 'comment_num_1', 'comment_num_2', 'comment_num_3', 'comment_num_4']]
-        pickle.dump(comments, open(dump_path, 'w'))
+        # df = pd.get_dummies(comments['comment_num'], prefix='comment_num')
+        # comments = pd.concat([comments, df], axis=1)
+        del comments['dt']
+        # comments = comments[['sku_id', 'has_bad_comment', 'bad_comment_rate', 'comment_num_1',
+        #                      'comment_num_2', 'comment_num_3', 'comment_num_4']]
+        # pickle.dump(comments, open(dump_path, 'w'))
     return comments
 
 
 def get_accumulate_user_feat(start_date, end_date):
     feature = ['user_id', 'user_action_1_ratio', 'user_action_2_ratio', 'user_action_3_ratio',
                'user_action_5_ratio', 'user_action_6_ratio']
-    dump_path = './cache/user_feat_accumulate_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/user_feat_accumulate_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
@@ -184,14 +187,14 @@ def get_accumulate_user_feat(start_date, end_date):
         actions['user_action_5_ratio'] = actions['action_4'] / actions['action_5']
         actions['user_action_6_ratio'] = actions['action_4'] / actions['action_6']
         actions = actions[feature]
-        pickle.dump(actions, open(dump_path, 'w'))
+        # pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
 def get_accumulate_product_feat(start_date, end_date):
     feature = ['sku_id', 'product_action_1_ratio', 'product_action_2_ratio', 'product_action_3_ratio',
                'product_action_5_ratio', 'product_action_6_ratio']
-    dump_path = './cache/product_feat_accumulate_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/product_feat_accumulate_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
@@ -205,12 +208,12 @@ def get_accumulate_product_feat(start_date, end_date):
         actions['product_action_5_ratio'] = actions['action_4'] / actions['action_5']
         actions['product_action_6_ratio'] = actions['action_4'] / actions['action_6']
         actions = actions[feature]
-        pickle.dump(actions, open(dump_path, 'w'))
+        # pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
 def get_labels(start_date, end_date):
-    dump_path = './cache/labels_%s_%s.pkl' % (start_date, end_date)
+    dump_path = './data.nosync/cache/labels_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
@@ -219,33 +222,33 @@ def get_labels(start_date, end_date):
         actions = actions.groupby(['user_id', 'sku_id'], as_index=False).sum()
         actions['label'] = 1
         actions = actions[['user_id', 'sku_id', 'label']]
-        pickle.dump(actions, open(dump_path, 'w'))
+        # pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
 
-def make_test_set(train_start_date, train_end_date):
-    dump_path = './cache/test_set_%s_%s.pkl' % (train_start_date, train_end_date)
+def make_test_set(test_start_date, test_end_date):
+    dump_path = './data.nosync/cache/test_set_%s_%s.pkl' % (test_start_date, test_end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
         start_days = "2016-02-01"
         user = get_basic_user_feat()
         product = get_basic_product_feat()
-        user_acc = get_accumulate_user_feat(start_days, train_end_date)
-        product_acc = get_accumulate_product_feat(start_days, train_end_date)
-        comment_acc = get_comments_product_feat(train_start_date, train_end_date)
+        user_acc = get_accumulate_user_feat(start_days, test_end_date)
+        product_acc = get_accumulate_product_feat(start_days, test_end_date)
+        comment_acc = get_comments_product_feat(test_start_date, test_end_date)
         #labels = get_labels(test_start_date, test_end_date)
 
         # generate 时间窗口
-        # actions = get_accumulate_action_feat(train_start_date, train_end_date)
+        # actions = get_accumulate_action_feat(test_start_date, test_end_date)
         actions = None
-        for i in (1, 2, 3, 5, 7, 10, 15, 21, 30):
-            start_days = datetime.strptime(train_end_date, '%Y-%m-%d') - timedelta(days=i)
+        for i in (21, 14, 7, 5, 3, 2, 1):
+            start_days = datetime.strptime(test_end_date, '%Y-%m-%d') - timedelta(days=i)
             start_days = start_days.strftime('%Y-%m-%d')
             if actions is None:
-                actions = get_action_feat(start_days, train_end_date)
+                actions = get_action_feat(start_days, test_end_date, i)
             else:
-                actions = pd.merge(actions, get_action_feat(start_days, train_end_date), how='left',
+                actions = pd.merge(actions, get_action_feat(start_days, test_end_date, i), how='left',
                                    on=['user_id', 'sku_id'])
 
         actions = pd.merge(actions, user, how='left', on='user_id')
@@ -253,17 +256,20 @@ def make_test_set(train_start_date, train_end_date):
         actions = pd.merge(actions, product, how='left', on='sku_id')
         actions = pd.merge(actions, product_acc, how='left', on='sku_id')
         actions = pd.merge(actions, comment_acc, how='left', on='sku_id')
-        #actions = pd.merge(actions, labels, how='left', on=['user_id', 'sku_id'])
+        # actions = pd.merge(actions, labels, how='left', on=['user_id', 'sku_id'])
         actions = actions.fillna(0)
         actions = actions[actions['cate'] == 8]
+        pickle.dump(actions, open(dump_path, 'w'))
 
     users = actions[['user_id', 'sku_id']].copy()
     del actions['user_id']
     del actions['sku_id']
     return users, actions
 
-def make_train_set(train_start_date, train_end_date, test_start_date, test_end_date, days=30):
-    dump_path = './cache/train_set_%s_%s_%s_%s.pkl' % (train_start_date, train_end_date, test_start_date, test_end_date)
+
+def make_train_set(train_start_date, train_end_date, result_start_date, result_end_date):
+    dump_path = './data.nosync/cache/train_set_%s_%s_%s_%s.pkl' % (train_start_date, train_end_date,
+                                                                   result_start_date, result_end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
@@ -273,18 +279,18 @@ def make_train_set(train_start_date, train_end_date, test_start_date, test_end_d
         user_acc = get_accumulate_user_feat(start_days, train_end_date)
         product_acc = get_accumulate_product_feat(start_days, train_end_date)
         comment_acc = get_comments_product_feat(train_start_date, train_end_date)
-        labels = get_labels(test_start_date, test_end_date)
+        labels = get_labels(result_start_date, result_end_date)
 
         # generate 时间窗口
         # actions = get_accumulate_action_feat(train_start_date, train_end_date)
         actions = None
-        for i in (1, 2, 3, 5, 7, 10, 15, 21, 30):
+        for i in (21, 14, 7, 5, 3, 2, 1):
             start_days = datetime.strptime(train_end_date, '%Y-%m-%d') - timedelta(days=i)
             start_days = start_days.strftime('%Y-%m-%d')
             if actions is None:
-                actions = get_action_feat(start_days, train_end_date)
+                actions = get_action_feat(start_days, train_end_date, i)
             else:
-                actions = pd.merge(actions, get_action_feat(start_days, train_end_date), how='left',
+                actions = pd.merge(actions, get_action_feat(start_days, train_end_date, i), how='left',
                                    on=['user_id', 'sku_id'])
 
         actions = pd.merge(actions, user, how='left', on='user_id')
@@ -294,6 +300,7 @@ def make_train_set(train_start_date, train_end_date, test_start_date, test_end_d
         actions = pd.merge(actions, comment_acc, how='left', on='sku_id')
         actions = pd.merge(actions, labels, how='left', on=['user_id', 'sku_id'])
         actions = actions.fillna(0)
+        pickle.dump(actions, open(dump_path, 'w'))
 
     users = actions[['user_id', 'sku_id']].copy()
     labels = actions['label'].copy()
@@ -304,60 +311,59 @@ def make_train_set(train_start_date, train_end_date, test_start_date, test_end_d
     return users, actions, labels
 
 
+def get_true(start_date, end_date):
+    actions = pd.read_csv('./data.nosync/JData_Action_201604.csv')
+    actions = actions[(actions.time >= start_date) & (actions.time < end_date)]
+    actions = actions[(actions['cate'] == 8) & (actions['type'] == 4)]
+    actions = actions.groupby(['user_id', 'sku_id'], as_index=False).sum()
+    return actions[['user_id', 'sku_id']]
+
+
 def report(pred, label):
 
-    actions = label
-    result = pred
-
-    # 所有用户商品对
-    all_user_item_pair = actions['user_id'].map(str) + '-' + actions['sku_id'].map(str)
+    # 所有购买的用户-商品对
+    all_user_item_pair = label['user_id'].map(str) + '-' + label['sku_id'].map(str)
     all_user_item_pair = np.array(all_user_item_pair)
-    # 所有购买用户
-    all_user_set = actions['user_id'].unique()
+    all_user_set = label['user_id'].unique()
+    print '用户-商品对：' + str(len(all_user_item_pair))
 
-    # 所有品类中预测购买的用户
-    all_user_test_set = result['user_id'].unique()
-    all_user_test_item_pair = result['user_id'].map(str) + '-' + result['sku_id'].map(str)
-    all_user_test_item_pair = np.array(all_user_test_item_pair)
+    # 预测购买的用户-商品对
+    all_user_pred_set = pred['user_id'].unique()
+    all_user_pred_item_pair = pred['user_id'].map(str) + '-' + pred['sku_id'].map(str)
+    all_user_pred_item_pair = np.array(all_user_pred_item_pair)
+    print '预测用户-商品对：' + str(len(all_user_pred_item_pair))
 
     # 计算所有用户购买评价指标
-    pos, neg = 0,0
-    for user_id in all_user_test_set:
+    pos = 0
+    for user_id in all_user_pred_set:
         if user_id in all_user_set:
             pos += 1
-        else:
-            neg += 1
-    all_user_acc = 1.0 * pos / ( pos + neg)
+    all_user_acc = 1.0 * pos / len(all_user_pred_set)
     all_user_recall = 1.0 * pos / len(all_user_set)
-    print '所有用户中预测购买用户的准确率为 ' + str(all_user_acc)
-    print '所有用户中预测购买用户的召回率' + str(all_user_recall)
+    print '预测购买用户的准确率：' + str(all_user_acc)
+    print '预测购买用户的召回率：' + str(all_user_recall)
 
-    pos, neg = 0, 0
-    for user_item_pair in all_user_test_item_pair:
+    pos = 0
+    for user_item_pair in all_user_pred_item_pair:
         if user_item_pair in all_user_item_pair:
             pos += 1
-        else:
-            neg += 1
-    all_item_acc = 1.0 * pos / ( pos + neg)
+    all_item_acc = 1.0 * pos / len(all_user_pred_item_pair)
     all_item_recall = 1.0 * pos / len(all_user_item_pair)
-    print '所有用户中预测购买商品的准确率为 ' + str(all_item_acc)
-    print '所有用户中预测购买商品的召回率' + str(all_item_recall)
+    print '预测购买商品的准确率：' + str(all_item_acc)
+    print '预测购买商品的召回率：' + str(all_item_recall)
     F11 = 6.0 * all_user_recall * all_user_acc / (5.0 * all_user_recall + all_user_acc)
     F12 = 5.0 * all_item_acc * all_item_recall / (2.0 * all_item_recall + 3 * all_item_acc)
     score = 0.4 * F11 + 0.6 * F12
-    print 'F11=' + str(F11)
-    print 'F12=' + str(F12)
-    print 'score=' + str(score)
+    print 'F11 = ' + str(F11)
+    print 'F12 = ' + str(F12)
+    print 'score = ' + str(score)
+
 
 if __name__ == '__main__':
-    train_start_date = '2016-02-01'
-    train_end_date = '2016-03-01'
-    test_start_date = '2016-03-01'
-    test_end_date = '2016-03-05'
-    user, action, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
+    train_start_date = '2016-03-08'
+    train_end_date = '2016-04-09'
+    result_start_date = '2016-04-09'
+    result_end_date = '2016-04-14'
+    user, action, label = make_train_set(train_start_date, train_end_date, result_start_date, result_end_date)
     print user.head(10)
     print action.head(10)
-
-
-
-
--- train.py
+++ train.py
@@ -1,82 +1,100 @@
-__author__ = 'foursking'
+__author__ = 'Created by foursking, edited by DPC'
 from gen_feat import make_train_set
 from gen_feat import make_test_set
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from gen_feat import report
+from gen_feat import get_true
 
 
-def xgboost_make_submission():
-    train_start_date = '2016-03-10'
-    train_end_date = '2016-04-11'
-    test_start_date = '2016-04-11'
+def xgboost_report_submission():
+    train_start_date = '2016-03-08'
+    train_end_date = '2016-04-09'
+    result_start_date = '2016-04-09'
+    result_end_date = '2016-04-14'
+
+    valid_start_date = '2016-03-01'
+    valid_end_date = '2016-04-02'
+    valid_result_start_date = '2016-04-02'
+    valid_result_end_date = '2016-04-07'
+
+    test_start_date = '2016-03-15'
     test_end_date = '2016-04-16'
 
-    sub_start_date = '2016-03-15'
-    sub_end_date = '2016-04-16'
-
-    user_index, training_data, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
-    X_train, X_test, y_train, y_test = train_test_split(training_data.values, label.values, test_size=0.2, random_state=0)
-    dtrain=xgb.DMatrix(X_train, label=y_train)
-    dtest=xgb.DMatrix(X_test, label=y_test)
-    param = {'learning_rate' : 0.1, 'n_estimators': 1000, 'max_depth': 3, 
-        'min_child_weight': 5, 'gamma': 0, 'subsample': 1.0, 'colsample_bytree': 0.8,
-        'scale_pos_weight': 1, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
+    user_index, training_data, label = make_train_set(train_start_date, train_end_date, result_start_date,
+                                                      result_end_date)
+    x_train, x_test, y_train, y_test = train_test_split(training_data.values,
+                                                        label.values, test_size=0.2, random_state=0)
+    dtrain = xgb.DMatrix(x_train, label=y_train)
+    dtest = xgb.DMatrix(x_test, label=y_test)
+    param = {'learning_rate': 0.1, 'n_estimators': 1000, 'max_depth': 3,
+             'min_child_weight': 5, 'gamma': 0, 'subsample': 1.0, 'colsample_bytree': 0.8,
+             'scale_pos_weight': 1, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
     num_round = 283
     param['nthread'] = 4
     #param['eval_metric'] = "auc"
     plst = param.items()
     plst += [('eval_metric', 'logloss')]
     evallist = [(dtest, 'eval'), (dtrain, 'train')]
-    bst=xgb.train(plst, dtrain, num_round, evallist)
-    sub_user_index, sub_trainning_data = make_test_set(sub_start_date, sub_end_date,)
-    sub_trainning_data = xgb.DMatrix(sub_trainning_data.values)
-    y = bst.predict(sub_trainning_data)
-    sub_user_index['label'] = y
-    pred = sub_user_index[sub_user_index['label'] >= 0.03]
-    pred = pred[['user_id', 'sku_id']]
-    pred = pred.groupby('user_id').first().reset_index()
-    pred['user_id'] = pred['user_id'].astype(int)
-    pred.to_csv('./sub/submission.csv', index=False, index_label=False)
+    bst = xgb.train(plst, dtrain, num_round, evallist)
 
+    # Report with validation set
+    valid_user_index, valid_trainning_date = make_test_set(valid_start_date, valid_end_date)
+    valid_trainning_date = xgb.DMatrix(valid_trainning_date.values)
+    pred_y = bst.predict(valid_trainning_date)
 
+    valid_pred = valid_user_index.copy()
+    valid_pred['label'] = pred_y
+    valid_pred = valid_pred[valid_pred['label'] >= 0.014]
+    valid_pred = valid_pred.sort_values('label', ascending=False).groupby('user_id').first().reset_index()
+    valid_true = get_true(valid_result_start_date, valid_result_end_date)
+    report(valid_pred, valid_true)
 
-def xgboost_cv():
-    train_start_date = '2016-03-05'
-    train_end_date = '2016-04-06'
-    test_start_date = '2016-04-11'
-    test_end_date = '2016-04-16'
+    valid_pred = valid_pred[valid_pred['label'] >= 0.016]
+    print 0.016
+    report(valid_pred, valid_true)
 
-    sub_start_date = '2016-02-05'
-    sub_end_date = '2016-03-05'
-    sub_test_start_date = '2016-03-05'
-    sub_test_end_date = '2016-03-10'
+    valid_pred = valid_pred[valid_pred['label'] >= 0.018]
+    print 0.018
+    report(valid_pred, valid_true)
 
-    user_index, training_data, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
-    X_train, X_test, y_train, y_test = train_test_split(training_data, label, test_size=0.2, random_state=0)
-    dtrain=xgb.DMatrix(X_train, label=y_train)
-    dtest=xgb.DMatrix(X_test, label=y_test)
-    param = {'max_depth': 10, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
-    num_round = 4000
-    param['nthread'] = 4
-    param['eval_metric'] = "auc"
-    plst = param.items()
-    plst += [('eval_metric', 'logloss')]
-    evallist = [(dtest, 'eval'), (dtrain, 'train')]
-    bst=xgb.train( plst, dtrain, num_round, evallist)
+    valid_pred = valid_pred[valid_pred['label'] >= 0.02]
+    print 0.02
+    report(valid_pred, valid_true)
 
-    sub_user_index, sub_trainning_date, sub_label = make_train_set(sub_start_date, sub_end_date,
-                                                                   sub_test_start_date, sub_test_end_date)
-    test = xgb.DMatrix(sub_trainning_date)
-    #y = bst.predict(test)
+    valid_pred = valid_pred[valid_pred['label'] >= 0.022]
+    print 0.022
+    report(valid_pred, valid_true)
 
-    pred = sub_user_index.copy()
-    y_true = sub_user_index.copy()
-    pred['label'] = y
-    y_true['label'] = label
-    report(pred, y_true)
+    valid_pred = valid_pred[valid_pred['label'] >= 0.024]
+    print 0.024
+    report(valid_pred, valid_true)
+
+    valid_pred = valid_pred[valid_pred['label'] >= 0.026]
+    print 0.026
+    report(valid_pred, valid_true)
+
+    valid_pred = valid_pred[valid_pred['label'] >= 0.028]
+    print 0.028
+    report(valid_pred, valid_true)
+
+    valid_pred = valid_pred[valid_pred['label'] >= 0.03]
+    print 0.03
+    report(valid_pred, valid_true)
+
+    # # Make submission for test set
+    # test_user_index, test_trainning_data = make_test_set(test_start_date, test_end_date)
+    # test_trainning_data = xgb.DMatrix(test_trainning_data.values)
+    # pred_y = bst.predict(test_trainning_data)
+    #
+    # pred = test_user_index.copy()
+    # pred['label'] = pred_y
+    # pred = pred[pred['label'] >= 0.022]
+    # pred = pred.sort_values('label', ascending=False).groupby('user_id').first().reset_index()
+    # pred = pred[['user_id', 'sku_id']]
+    # pred['user_id'] = pred['user_id'].astype(int)
+    # pred.to_csv('./sub/submission.csv', index=False)
 
 
 if __name__ == '__main__':
-    #xgboost_cv()
-    xgboost_make_submission()
+    xgboost_report_submission()
